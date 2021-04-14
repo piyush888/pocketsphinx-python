5bfcef8b77b7a996218bbfd1e2ed56b9310a6d33
Piyush <piyush888@gmail.com>
2021-04-14 13:59:29 +0530

    shell script modified

--- pocketsphinx.sh
+++ pocketsphinx.sh
@@ -1,6 +1,6 @@
 brew reinstall swig
 git clone --recursive https://github.com/piyush888/pocketsphinx-python.git
 cd pocketsphinx-python
-sed -i.bak 's/\b#include <al.h>\b/\b#include <OpenAL/al.h>\b/' deps/sphinxbase/src/libsphinxad/ad_openal.c
-sed -i.bak 's/\b#include <alc.h>\b/\b#include <OpenAL/alc.h>\b/' deps/sphinxbase/src/libsphinxad/ad_openal.c
+sed -i.bak 's/\#include \<al.h\>/\#include \<OpenAL\/al.h\>/' deps/sphinxbase/src/libsphinxad/ad_openal.c
+sed -i.bak 's/\#include \<alc.h\>/\#include \<OpenAL\/alc.h\>/' deps/sphinxbase/src/libsphinxad/ad_openal.c
 python setup.py install
